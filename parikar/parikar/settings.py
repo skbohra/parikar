c1f21059e1d70742614e8fc46de6d860eb44cf58
shreekant <shreekant@geekybuddha.org>
2024-02-29 17:50:02 +0530

    bug fixing

--- parikar/parikar/settings.py
+++ parikar/parikar/settings.py
@@ -208,10 +208,10 @@ except ImportError:
     }
     
     #DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    AWS_STORAGE_BUCKET_NAME = os.enviorn['BUCKET_NAME']
-    AWS_S3_ENDPOINT_URL = os.enviorn['S3_ENDPOINT_URL']
-    AWS_S3_ACCESS_KEY_ID = os.enviorn['S3_ACCESS_KEY_ID']
-    AWS_S3_SECRET_ACCESS_KEY = os.enviorn['S3_ACCESS_KEY']
+    AWS_STORAGE_BUCKET_NAME = os.environ['BUCKET_NAME']
+    AWS_S3_ENDPOINT_URL = os.environ['S3_ENDPOINT_URL']
+    AWS_S3_ACCESS_KEY_ID = os.environ['S3_ACCESS_KEY_ID']
+    AWS_S3_SECRET_ACCESS_KEY = os.environ['S3_ACCESS_KEY']
     AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 
