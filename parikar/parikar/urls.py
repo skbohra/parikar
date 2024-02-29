e64dbb35d856150cccd29651f6c6dd4836b8e440
shreekant <shreekant@geekybuddha.org>
2024-02-29 08:24:14 +0530

    adding support for RAILWAY VOLUMES FOR MEDIA

--- parikar/parikar/urls.py
+++ parikar/parikar/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf import settings
 
 
@@ -31,5 +31,5 @@ urlpatterns = [
 from django.conf.urls.static import static
 
 urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns = urlpatterns  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+urlpatterns += [ re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})]
