61e88cb6dda979512c2fe360b79ad02081dd5fac
shreekant <shreekant@geekybuddha.org>
2024-02-29 11:30:34 +0530

    correcting whitenoise configuration

--- parikar/parikar/urls.py
+++ parikar/parikar/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
 
 from django.conf.urls.static import static
 
-urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#if settings.DEBUG:
+#    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += [ re_path(r'^mymedia/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})]
