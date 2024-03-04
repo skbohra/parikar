f5806f6c3d05bece3b93d26f83abaab6a07b0bdc
shreekant <shreekant@geekybuddha.org>
2024-03-04 16:50:55 +0530

    removed chrome-plugin from git repo due to issue with railway deployment

--- parikar-chrome-plugin/background.js
+++
@@ -1,9 +0,0 @@
-
-
-chrome.action.onClicked.addListener((tab) => {
-  if (!tab.url.includes('chrome://')) {
-	var new_url = "https://www.parikar.org/play/instant/?url="+tab.url;
-	chrome.tabs.create({ url: new_url });
-  }
-});
-
