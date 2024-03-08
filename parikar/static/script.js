54347a277e1348d1faeecabb4d853b77f20ad2ef
shreekant <shreekant@geekybuddha.org>
2024-03-08 14:46:37 +0530

    using notify.js for django messages display

--- parikar/static/script.js
+++ parikar/static/script.js
@@ -13,4 +13,16 @@ $(".search-button").click(function(e){
 
 	$(this).parent().submit();
 });
+var success_message = $(".success");
+var info_message = $(".error");
+var error_message = $(".info");
+
+$.notify(success_message.text(),"success");
+$.notify(info_message.text(),"info");
+$.notify(error_message.text(),"error");
+success_message.parent().hide();
+error_message.parent().hide();
+info_message.parent().hide();
+
+
 });
