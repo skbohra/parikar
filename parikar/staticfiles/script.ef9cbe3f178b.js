21d40157b4d2b6f679ecaff12152584c5a4649fe
shreekant <shreekant@geekybuddha.org>
2024-03-08 16:37:17 +0530

    added missing files

---
+++ parikar/staticfiles/script.ef9cbe3f178b.js
@@ -0,0 +1,28 @@
+var menuIcon = document.querySelector(".menu-icon");
+var sidebar = document.querySelector(".sidebar");
+var container = document.querySelector(".container");
+
+menuIcon.onclick = function(){
+    sidebar.classList.toggle("small-sidebar");
+    container.classList.toggle("large-container");
+}
+
+
+$(function(){
+$(".search-button").click(function(e){
+
+	$(this).parent().submit();
+});
+var success_message = $(".success");
+var info_message = $(".error");
+var error_message = $(".info");
+
+$.notify(success_message.text(),"success");
+$.notify(info_message.text(),"info");
+$.notify(error_message.text(),"error");
+success_message.parent().hide();
+error_message.parent().hide();
+info_message.parent().hide();
+
+
+});
