54ee1acec1a154d9e0ab06d306b5660806affdc5
shreekant <shreekant@geekybuddha.org>
2024-03-12 22:04:14 +0530

    added color widget to color field

--- parikar/main/forms.py
+++ parikar/main/forms.py
@@ -1,10 +1,15 @@
 from django.forms import ModelForm
 from .models import * 
+from django.forms.widgets import TextInput
 
 class NewParikForm(ModelForm):
     class Meta:
         model = Parik
         exclude = ["user","created_on"]
+        widgets = {
+                   'color': TextInput(attrs={'type': 'color'}),
+                   'background': TextInput(attrs={'type': 'color'}),
+                   }
 
 class NewChannelForm(ModelForm):
     class Meta:
